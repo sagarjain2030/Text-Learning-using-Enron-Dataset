4f3bd8745e34657c73c6044687a11b282f93841b
sagarjain2030 <sagarjain2030@gmail.com>
2018-01-09 19:16:08 +0530

    feat : Code Naive Bayes
    Commit includes Coding of naive Bayes theorem and printing values required to solve questions asked in Lesson 2.
    
    On branch lesson
    Changes to be committed:
    	modified:   naive_bayes/nb_author_id.py

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -27,7 +27,30 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+#Importing GaussianNB
+from sklearn.naive_bayes import GaussianNB
 
+#Creating Classifier
+clf = GaussianNB()
+
+#Finding time required to fit classifier
+t0 = time()
+
+#Fit the classifier
+clf.fit(features_train,labels_train)
+
+#Calculating time required by classifier to Fit
+print "training time:", round(time()-t0, 3), "s"
+
+#Finding time required to predict
+tp = time()
+
+#Accuracy of classifier
+print("The accuracy of classifier is ")
+print(clf.score(features_test,labels_test))
+
+#Calculating time required by classifier to Predict
+print "testing time:", round(time()-tp, 3), "s"
 #########################################################
 
 
